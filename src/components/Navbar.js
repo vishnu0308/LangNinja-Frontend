595687d0806f89d6e8964f089e782ebc3d5a0491
Vishnu Shravan <vishnushravan007@gmail.com>
2023-10-23 18:58:52 +0530

    Navbar for mobile responsiveness
    Signed-off-by: Vishnu Shravan <vishnushravan007@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,68 +1,134 @@
-import React, { useContext } from 'react';
-import AppBar from '@mui/material/AppBar'; // This is the top bar of the website.
-import Toolbar from '@mui/material/Toolbar'; // A container for the Navbar.
-import Typography from '@mui/material/Typography'; // For adding text to the Navbar.
-import Button from '@mui/material/Button'; // For creating buttons.
-import { Link } from 'react-router-dom'; // To navigate to different pages.
+import React, { useContext, useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu'; // Import the Menu icon
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { Link } from 'react-router-dom';
+import Hidden from '@mui/material/Hidden';
 import UserStore from '../contexts/UserStore';
 import Logout from './signout';
 
 function Navbar() {
   const titleStyle = {
-    textDecoration: 'none', // Remove the underline typically associated with links.
-    color: 'inherit', // Use the same color as the surrounding text.
-    cursor: 'pointer', // Change the cursor to a pointer to indicate it's clickable.
+    textDecoration: 'none',
+    color: 'inherit',
+    cursor: 'pointer',
   };
 
   const navbarStyle = {
-    width: '100%', // Set the width to 100% to occupy the entire horizontal width.
+    width: '100%',
     display: 'flex',
     justifyContent: 'center',
   };
 
-  const {getSession} = useContext(UserStore);
+  const { getSession } = useContext(UserStore);
+
+  // State to control the mobile menu
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const openMobileMenu = () => {
+    setMobileMenuOpen(true);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
     <AppBar position="static" color="primary" style={navbarStyle}>
-      {/* The AppBar holds the Navbar and is positioned at the top. It has a blue background. */}
       <Toolbar>
-        {/* The Toolbar contains the Navbar content. */}
-        <Typography variant="h6" style={{ flexGrow: 1 }} >
-          {/* Typography is for text. This is our website's title. */}
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
           <Link to="/" style={titleStyle}>
             LangNinja
           </Link>
         </Typography>
-        {getSession() ? (
-          /* If the user is logged in, show these buttons. */
+
+        {/* Hamburger menu for mobile */}
+        <Hidden mdUp>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={openMobileMenu}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+
+        {/* Desktop navigation */}
+        <Hidden smDown>
           <div>
-            <Button component={Link} to="/admin" color="inherit">
-              My progress
-            </Button>
-            <Button component={Link} to="/stats" color="inherit">
-              My Stats
-            </Button>
-            <Button component={Link} to="/leaderboard" color="inherit">
-              Leaderboard
-            </Button>
-            <Button component={Link} to="/settings" color="inherit">
-              Settings
-            </Button>
-            <Button color="secondary">
-              {<Logout />}
-            </Button>
+            {getSession() ? (
+              <div>
+                <Button component={Link} to="/admin" color="inherit">
+                  My progress
+                </Button>
+                <Button component={Link} to="/stats" color="inherit">
+                  My Stats
+                </Button>
+                <Button component={Link} to="/leaderboard" color="inherit">
+                  Leaderboard
+                </Button>
+                <Button component={Link} to="/settings" color="inherit">
+                  Settings
+                </Button>
+                <Button color="secondary">
+                  {<Logout />}
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <Button component={Link} to="/signup" color="inherit">
+                  Sign Up
+                </Button>
+                <Button component={Link} to="/signin" color="inherit">
+                  Sign In
+                </Button>
+              </div>
+            )}
           </div>
-        ) : (
-          /* If the user is not logged in, show this button. */
-          <div>
-              <Button component={Link} to="/signup" color="inherit">
-                Sign Up
-              </Button>
-              <Button component={Link} to="/signin" color="inherit">
-                Sign In
-              </Button>
-          </div>
-        )}
+        </Hidden>
       </Toolbar>
+
+      {/* Mobile menu */}
+      <Drawer anchor="right" open={mobileMenuOpen} onClose={closeMobileMenu}>
+        <List>
+          {getSession() ? (
+            <div>
+              <ListItem button component={Link} to="/admin">
+                <ListItemText primary="My progress" />
+              </ListItem>
+              <ListItem button component={Link} to="/stats">
+                <ListItemText primary="My Stats" />
+              </ListItem>
+              <ListItem button component={Link} to="/leaderboard">
+                <ListItemText primary="Leaderboard" />
+              </ListItem>
+              <ListItem button component={Link} to="/settings">
+                <ListItemText primary="Settings" />
+              </ListItem>
+              <ListItem button>
+                {<Logout />}
+              </ListItem>
+            </div>
+          ) : (
+            <div>
+              <ListItem button component={Link} to="/signup">
+                <ListItemText primary="Sign Up" />
+              </ListItem>
+              <ListItem button component={Link} to="/signin">
+                <ListItemText primary="Sign In" />
+              </ListItem>
+            </div>
+          )}
+        </List>
+      </Drawer>
     </AppBar>
   );
 }
